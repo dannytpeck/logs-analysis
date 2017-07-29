6608a6ab66515fb15d09970ab639dd3dd958c919
dannytpeck <dannytpeck@gmail.com>
2017-07-29 19:05:14 +0000

    Refactor based on feedback

--- analyze.py
+++ analyze.py
@@ -1,36 +1,43 @@
-#!/usr/bin/env python2.7
+#!/usr/bin/env python
 
 import psycopg2
 
-# Connect to news db and start cursor
-db = psycopg2.connect("dbname=news")
-c = db.cursor()
+def connect(dbname):
+    try:
+        db = psycopg2.connect("dbname={}".format(dbname))
+        cursor = db.cursor()
+        return db, cursor
+    except:
+        db = None;
+        print("Unable to connect to {} database.".format(dbname))
 
+# Connect to news db and start cursor
+db, cursor = connect("news")
 
 # What are the most popular three articles of all time?
 def three_most_popular_articles():
-    c.execute('''select title, count(*) as views
+    cursor.execute('''select title, count(*) as views
         from article_views
         group by title
         order by views desc
         limit 3''')
-    return c.fetchall()
+    return cursor.fetchall()
 
 
 # Who are the most popular article authors of all time?
 def most_popular_authors():
-    c.execute('''select authors.name, count(*) as views
+    cursor.execute('''select authors.name, count(*) as views
         from authors, article_views
         where authors.id = article_views.author
         group by authors.name
         order by views desc''')
-    return c.fetchall()
+    return cursor.fetchall()
 
 
 # On which days did more than 1% of requests lead to errors?
 def most_daily_errors():
-    c.execute('''select * from error_rate where error_percent > 1''')
-    return c.fetchall()
+    cursor.execute('''select * from error_rate where error_percent > 1''')
+    return cursor.fetchall()
 
 
 # Print the three queries, nicely formatted
@@ -47,4 +54,5 @@ print
 
 
 # Close db now that we're done with it
-db.close()
+if (db):
+    db.close()
