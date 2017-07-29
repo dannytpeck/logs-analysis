86ae223cbc87460f4be69c4b5492caf92ef839fd
dannytpeck <dannytpeck@gmail.com>
2017-07-29 08:32:10 +0000

    Add queries for question 2 and 3

--- analyze.py
+++ analyze.py
@@ -1,18 +1,44 @@
 import psycopg2
 
-def query_one():
-    db = psycopg2.connect("dbname=news")
-    c = db.cursor()
-    c.execute('''select articles.title, count(*) as views
-                 from articles, log
-                 where log.path like '%' || articles.slug
-                 group by articles.title
-                 order by views desc''')
+db = psycopg2.connect("dbname=news")
+c = db.cursor()
+
+
+# What are the most popular three articles of all time?
+def three_most_popular_articles():
+    c.execute('''select title, count(*) as views
+        from article_views
+        group by title
+        order by views desc
+        limit 3''')
     return c.fetchall()
-    db.close()
 
-def query_two():
 
-results1 = query_one()
-for row in results1:
+# Who are the most popular article authors of all time?
+def most_popular_authors():
+    c.execute('''select authors.name, count(*) as views
+        from authors, article_views
+        where authors.id = article_views.author
+        group by authors.name
+        order by views desc''')
+    return c.fetchall()
+
+
+# On which days did more than 1% of requests lead to errors?
+def most_daily_errors():
+    c.execute('''select * from error_rate where error_percent > 1''')
+    return c.fetchall()
+
+
+print
+for row in three_most_popular_articles():
     print "\"{}\" - {} views".format(row[0], row[1])
+print
+for row in most_popular_authors():
+    print "{} - {} views".format(row[0], row[1])
+print
+for row in most_daily_errors():
+    print "{:%B %d, %Y} - {}% errors".format(row[0], row[1])
+print
+
+db.close()
